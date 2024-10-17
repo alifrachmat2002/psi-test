62161a86f35901c6690c69c70176f2ea79ef4016
alifrachmat2002 <aaliplagi@gmail.com>
2024-10-17 13:39:41 +0700

    Refactor: Update CSS file reference in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
     <!-- Custom styles for this template -->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.css') }}" rel="stylesheet">
 
 </head>
 
